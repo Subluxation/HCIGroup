ea8b3270634c8b8e3bc1181351694c06c7960558
dendrio405 <anthony40555@gmail.com>
2017-04-05 23:07:05 -0500

    Added a main screen before the actual game. Consists of a single Play button. I also made it so that once the game ends, it takes you back to the main screen.

--- Documents/workspace/BalloonGame/src/jfx/BalloonView.java
+++ Documents/workspace/BalloonGame/src/jfx/BalloonView.java
@@ -6,8 +6,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -19,12 +23,30 @@ public class BalloonView extends Application
 	
 	public void start(Stage stage) throws Exception 
 	{
-		//Each button on the menu starts a different manager. The GamePlayManager is the one that is called with the 'play' button 
+		Pane pane = new Pane();
 		
-		GamePlayManager manager=new GamePlayManager(stage);
+		Scene mainScene = new Scene(pane, 800, 800);
 		
-		manager.start();
+		VBox box = new VBox();
+		box.setAlignment(Pos.CENTER);
+		box.setSpacing(100);
+		box.setMinSize(pane.getWidth(), pane.getHeight());
 		
+		Button playButton = new Button("Play");
+		playButton.setPadding(new Insets(10, 50, 10, 50));
+		playButton.setOnAction((e)->
+		{
+			//Each button on the menu starts a different manager. The GamePlayManager is the one that is called with the 'play' button 
+			
+			GamePlayManager manager=new GamePlayManager(stage, mainScene);
+			
+			manager.start();
+		});
+
+		box.getChildren().add(playButton);
+		pane.getChildren().add(box);
+		
+		stage.setScene(mainScene);
 		stage.show();
 	}
 	
